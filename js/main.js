ecbfb148fc42791cc2a80e676607b6d8de226580
patsalay <patsalay@quartsoft.com>
2018-05-03 15:05:08 +0300

    images optimized

--- js/main.js
+++ js/main.js
@@ -46,7 +46,7 @@ new Vue({
                 {
                     name: 'Гердан Бирюзовые завитки на черном',
                     dscr: '',
-                    img: 'images/products/2017.11.11-2.jpg',
+                    img: 'images/products/2017.11.11-2_981x773.jpg',
                     category: 'gerdan'
                 },
                 {
@@ -70,7 +70,7 @@ new Vue({
                 {
                     name: 'Жгут Греческий (зеленый)',
                     dscr: 'Длина около 45 см. Диаметр около 15 мм.',
-                    img: 'images/products/2016.12.16_грек зеленый.jpg',
+                    img: 'images/products/2016.12.16_grek zelenyiy_1296x972.jpg',
                     category: 'rope'
                 },
                 {
@@ -88,49 +88,49 @@ new Vue({
                 {
                     name: 'Гердан Бирюзовый',
                     dscr: 'Ширина центрального элемента около 6 см.',
-                    img: 'images/products/2016.10.29_астафьева бирюзовый (1).jpg',
+                    img: 'images/products/2017.11.11-2_981x773.jpg',
                     category: 'gerdan'
                 },
                 {
                     name: 'Гердан Чернобрывец',
                     dscr: 'Длина около 53 см. Ширина центрального элемента около 6 см.',
-                    img: 'images/products/2016.09.24_гердан чернобрывец.jpg',
+                    img: 'images/products/2016.09.24_gerdan chernobryivets_1296x972.jpg',
                     category: 'gerdan'
                 },
                 {
                     name: 'Жгут из бисера №69',
                     dscr: 'Длина около 42 см. Диаметр около 12мм',
-                    img: 'images/products/2016.09.17_капралова_69.jpg',
+                    img: 'images/products/2016.09.17_kapralova_69_1296x972.jpg',
                     category: 'rope'
                 },
                 {
                     name: 'Гердан Черно-красно-золотой',
                     dscr: 'Длина около 53 см. Ширина центрального элемента около 6,5 см.',
-                    img: 'images/products/2016.08.14_гердан_крас черн зол (1).jpg',
+                    img: 'images/products/2016.08.14_gerdan_kras chern zol (1)_1296x972.jpg',
                     category: 'gerdan'
                 },
                 {
                     name: 'Жгут Греческий черно-белый',
                     dscr: 'Длина около 42 см.',
-                    img: 'images/products/2016.07.16_чернобелый грек.jpg',
+                    img: 'images/products/2016.07.16_chernobelyiy grek_1296x972.jpg',
                     category: 'rope'
                 },
                 {
                     name: 'Жгут из мелкого японского бисера Изморозь',
                     dscr: 'Длина около 43 см, диаметр около 12мм.',
-                    img: 'images/products/2016.06.21_изморозь (1).jpg',
+                    img: 'images/products/2016.06.21_izmorozy (1)_1296x972.jpg',
                     category: 'rope'
                 },
                 {
                     name: 'Жгут Маки Ди',
                     dscr: 'Длина около 45 см.',
-                    img: 'images/products/2016.06.06_повтор маки ди (1).jpg',
+                    img: 'images/products/2016.06.06_povtor maki di (1)_1296x972.jpg',
                     category: 'rope'
                 },
                 {
                     name: 'Жгут Барберри',
                     dscr: 'Длина около 45 см, диаметр около 12мм.',
-                    img: 'images/products/2016.05.11_барбери (1).jpg',
+                    img: 'images/products/2016.05.11_barberi (1)_1296x972.jpg',
                     category: 'rope'
                 },
                 {
@@ -182,6 +182,12 @@ new Vue({
                     category: 'rope'
                 },
                 {
+                    name: 'Лариат из рубки',
+                    dscr: 'Длина около 140 см.',
+                    img: 'images/products/2015.07.26_lariat iz rubki (4)_743x839.jpg',
+                    category: 'rope'
+                },
+                {
                     name: 'Лариат меланжевый фиолетовый',
                     dscr: 'Длина около 145 см.',
                     img: 'images/products/2015.06.28_лариат меланж.jpg',
@@ -202,13 +208,13 @@ new Vue({
                 {
                     name: 'Жгут Кобальтовые квадраты',
                     dscr: 'Длина около 42 см.',
-                    img: 'images/products/2015.04.22_кобальтовые квадраты.jpg',
+                    img: 'images/products/2015.04.22_kobalytovyie kvadratyi_981x775.jpg',
                     category: 'rope'
                 },
                 {
                     name: 'Жгут с переходом цвета (зеленый)',
                     dscr: 'Длина около 45 см.',
-                    img: 'images/products/2015.04.09_переход.jpg',
+                    img: 'images/products/2015.04.09_perehod_981x744.jpg',
                     category: 'rope'
                 },
                 {
@@ -220,13 +226,13 @@ new Vue({
                 {
                     name: 'Жгут Клевер',
                     dscr: 'Длина около 45 см.',
-                    img: 'images/products/2015.02.01_клевер (3).jpg',
+                    img: 'images/products/2015.02.01_klever (3)_981x978.jpg',
                     category: 'rope'
                 },
                 {
                     name: 'Жгут Греческий (золотой)',
                     dscr: 'Длина около 42 см.',
-                    img: 'images/products/2015.01.10_греческий 1.jpg',
+                    img: 'images/products/2015.01.10_grecheskiy 1_981x950.jpg',
                     category: 'rope'
                 },
                 {
